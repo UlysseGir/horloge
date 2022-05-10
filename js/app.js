ca302a8d8fae4311ef83094f1b7e1345c57b295b
lecanardjaune <ugiraud@edenschool.fr>
2022-05-10 17:20:14 +0200

    save project

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let second = 0;
+let num = document.getElementsByClassName("num");
+
+
+let secondInt = setInterval(function(){
+    secondNum();
+}, 1000);
+
+function secondNum(){
+    second++;
+    console.log(second);
+
+    for(let i=0; i < num.length; i++){
+        num[i].style.background = "#1f1f1f"
+    }
+
+    for(let i=0; i < num.length; i++){
+
+        if(num[i].classList[3].includes(second.toString())){
+            num[i].style.background = "red"
+        }
+        
+    }
+
+
+}
