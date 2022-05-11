4cc3b7ad7c88602fa84c8ca977e3331cfb28ed5f
lecanardjaune <ugiraud@edenschool.fr>
2022-05-11 10:54:11 +0200

    js finish

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 let secondUnits = 0;
 let secondTens = 0;
 let minuteUnits = 0;
+let minuteTens = 0;
 let num = document.getElementsByClassName("num");
 let unit = document.getElementsByClassName("unit");
 let tens = document.getElementsByClassName("tens");
+let mUnit = document.getElementsByClassName("mUnit");
+let mTens = document.getElementsByClassName("mTens");
 
 
 let secondInt = setInterval(function(){
@@ -11,11 +14,9 @@ let secondInt = setInterval(function(){
 }, 1000);
 
 function secondNum(){
-    
-    console.log(secondUnits);
 
     for(let i=0; i < num.length; i++){
-        num[i].style.background = "#1f1f1f"
+        num[i].style.background = "#0e0e0e"
     }
 
     for(let i=0; i < unit.length; i++){
@@ -23,14 +24,22 @@ function secondNum(){
         if(unit[i].classList[3].includes(secondUnits.toString())){
             unit[i].style.background = "red"
         }
-        
     }
     for(let i=0; i < tens.length; i++){
 
         if(tens[i].classList[3].includes(secondTens.toString())){
             tens[i].style.background = "red"
         }
-        
+    }
+    for(let i=0; i < mUnit.length; i++){
+        if(mUnit[i].classList[3].includes(minuteUnits.toString())){
+            mUnit[i].style.background = "red"
+        }
+    }
+    for(let i=0; i < mTens.length; i++){
+        if(mTens[i].classList[3].includes(minuteTens.toString())){
+            mTens[i].style.background = "red"
+        }
     }
 
     
@@ -42,6 +51,10 @@ function secondNum(){
     if(secondTens == 6){
         secondTens = 0;
         minuteUnits++;
+    }
+    if(minuteUnits == 9){
+        minuteUnits = 0;
+        minuteTens++;
     }
 
     secondUnits++;
